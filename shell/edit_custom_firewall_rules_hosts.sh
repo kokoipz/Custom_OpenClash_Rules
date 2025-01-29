38edd07d43f7edfab8a87929819ecebdf3fdcd0d
kokoipz <106084614+kokoipz@users.noreply.github.com>
2025-01-29 14:56:53 +0800

    Update edit_custom_firewall_rules_hosts.sh

--- shell/edit_custom_firewall_rules_hosts.sh
+++ shell/edit_custom_firewall_rules_hosts.sh
@@ -9,7 +9,7 @@ INSERT_CONTENT=$(cat << EOF
 # 以下是 GitHub520 加速规则拉取脚本
 LOG_OUT "拉取GitHub520加速规则…"
 sed -i '/# GitHub520 Host Start/,/# GitHub520 Host End/d' /etc/hosts
-curl https://ghkkz.440222.xyz/https://raw.hellogithub.com/hosts >> /etc/hosts
+curl https://raw.hellogithub.com/hosts >> /etc/hosts
 sed -i '/^$/d' /etc/hosts
 sed -i '/!/d' /etc/hosts
 # GitHub520 加速规则拉取脚本结束
@@ -19,7 +19,7 @@ LOG_OUT "清理 DNS 缓存…"
 # 以下是广告过滤规则拉取脚本
 LOG_OUT "拉取秋风广告过滤规则…"
 sed -i '/# AWAvenue-Ads-Rule Start/,/# AWAvenue-Ads-Rule End/d' /etc/hosts
-curl https://ghkkz.440222.xyz/https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-hosts.txt | \
+curl https://github.boki.moe/https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-hosts.txt | \
     sed '/127.0.0.1 localhost/d; /::1 localhost/d; 1s/^/# AWAvenue-Ads-Rule Start\n/; $s/$/\n# AWAvenue-Ads-Rule End/' >> /etc/hosts
 sed -i '/^$/d' /etc/hosts
 sed -i '/!/d' /etc/hosts
